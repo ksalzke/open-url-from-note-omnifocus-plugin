36903c0985497417de7b7e2b555488360bdd4395
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-18 21:35:37 +1100

    feat: :lipstick: add preferences dialogue

---
+++ OpenURL.omnifocusjs/Resources/preferences.js
@@ -0,0 +1,22 @@
+/* global PlugIn Form */
+(() => {
+  const action = new PlugIn.Action(async function (selection, sender) {
+    // get current preferences or set defaults if they don't yet exist
+    const omniFocusOpen = this.openURLlib.getOmniFocusOpenPref()
+
+    // create and show form
+    const form = new Form()
+    form.addField(new Form.Field.Option('omniFocusOpen', 'Open OmniFocus Links In...', ['window', 'tab', 'neither'], ['New window', 'New tab', 'Current window and tab'], omniFocusOpen))
+    await form.show('Preferences: Open URL(s)', 'OK')
+
+    // save preferences
+    const syncedPrefs = this.openURLlib.loadSyncedPrefs()
+    syncedPrefs.write('omniFocusOpen', form.values.omniFocusOpen)
+  })
+
+  action.validate = function (selection, sender) {
+    return true
+  }
+
+  return action
+})()
