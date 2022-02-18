71383fa44b93c0f4b58f0b822a8c13f18a444700
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-18 12:07:13 +1100

    open OmniFocus URLs in new window

--- OpenURL.omnifocusjs/Resources/openURLlib.js
+++ OpenURL.omnifocusjs/Resources/openURLlib.js
@@ -2,5 +2,15 @@
 (() => {
   const openURLlib = new PlugIn.Library(new Version('1.0'))
 
+  openURLlib.openURL = url => {
+    if (url.toLowerCase().startsWith('omnifocus://')) openURLlib.openOmniFocusURL(url)
+    else URL.fromString(url).open()
+  }
+
+  openURLlib.openOmniFocusURL = async url => {
+    await document.newWindow()
+    URL.fromString(url).open()
+  }
+
   return openURLlib
 })()
--- OpenURL.omnifocusjs/Resources/openurl.js
+++ OpenURL.omnifocusjs/Resources/openurl.js
@@ -1,24 +1,27 @@
 /* global PlugIn Form */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
+    const lib = this.openURLlib
     const task = selection.tasks[0]
 
     const regex = /\b\S*\:\/{2,3}[a-zA-Z./?_=\-\&%0-9~#\[\]!\$'\(\)\*\+]*(\b)?/gm // eslint-disable-line
 
     const urls = [...task.note.matchAll(regex)].map(match => match[0])
 
-    if (urls.length === 1) URL.fromString(urls[0]).open()
+    // TODO: Alert if no URL found
+
+    if (urls.length === 1) lib.openURL(urls[0])
 
     else if (urls.length > 1) {
       const form = new Form()
       form.addField(new Form.Field.MultipleOptions('urls', 'URLs', urls, urls, urls))
       await form.show('Select URLs to Open', 'Open')
-      for (const url of form.values.urls) URL.fromString(url).open()
+      for (const url of form.values.urls) lib.openURL(url)
     }
   })
 
   action.validate = function (selection, sender) {
-    return selection.tasks.length > 0
+    return selection.tasks.length === 1
   }
 
   return action
