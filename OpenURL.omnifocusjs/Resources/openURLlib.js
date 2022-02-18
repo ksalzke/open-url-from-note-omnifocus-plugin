a3d5bb8d31a5d14a57a825d4c237f9c5457d3050
Kaitlin <kaitlinsalzke@gmail.com>
2022-02-18 21:44:54 +1100

    feat: ✨ allow user to choose behaviour for OmniFocus links (#1)
    * open OmniFocus URLs in new window
    
    * feat: :lipstick: add preferences dialogue
    
    * refactor: :lipstick: update preference label
    
    * feat: :sparkles: allow user to choose behaviour for OmniFocus links - new tab, new window, or current window & tab

--- OpenURL.omnifocusjs/Resources/openURLlib.js
+++ OpenURL.omnifocusjs/Resources/openURLlib.js
@@ -1,6 +1,45 @@
-/* global PlugIn Version */
+/* global PlugIn Version Alert */
 (() => {
   const openURLlib = new PlugIn.Library(new Version('1.0'))
+
+  openURLlib.loadSyncedPrefs = () => {
+    const syncedPrefsPlugin = PlugIn.find('com.KaitlinSalzke.SyncedPrefLibrary')
+
+    if (syncedPrefsPlugin !== null) {
+      const SyncedPref = syncedPrefsPlugin.library('syncedPrefLibrary').SyncedPref
+      return new SyncedPref('com.KaitlinSalzke.OpenURL')
+    } else {
+      const alert = new Alert(
+        'Synced Preferences Library Required',
+        'For the \'Open URL(s)\' plug-in to work correctly, the \'Synced Preferences for OmniFocus\' plugin(https://github.com/ksalzke/synced-preferences-for-omnifocus) is also required and needs to be added to the plug-in folder separately. Either you do not currently have this plugin installed, or it is not installed correctly.'
+      )
+      alert.show()
+    }
+  }
+
+  openURLlib.getOmniFocusOpenPref = () => {
+    const syncedPrefs = openURLlib.loadSyncedPrefs()
+    const pref = syncedPrefs.readString('omniFocusOpen')
+    if (pref === null) return 'neither' // pref has not been set, return neither as default
+    else if (['window', 'tab', 'neither'].includes(pref)) return pref // pref has been set
+    else {
+      // pref has somehow been set to something else, return default and set back to neither
+      syncedPrefs.write('omniFocusOpen', 'neither')
+      return 'neither'
+    }
+  }
+
+  openURLlib.openURL = url => {
+    if (url.toLowerCase().startsWith('omnifocus://')) openURLlib.openOmniFocusURL(url)
+    else URL.fromString(url).open()
+  }
+
+  openURLlib.openOmniFocusURL = async url => {
+    const pref = openURLlib.getOmniFocusOpenPref()
+    if (pref === 'window') await document.newWindow()
+    else if (pref === 'tab') await document.newTabOnWindow(document.windows[0])
+    URL.fromString(url).open()
+  }
 
   return openURLlib
 })()
